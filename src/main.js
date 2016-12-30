a086053e5e172e1c7fcd57ed23f10fb77a02c32b
Rick Wong <wong@mollie.com>
2016-12-31 00:03:42 +0100

    Showing containers and resizing etc

--- src/main.js
+++ src/main.js
@@ -3,28 +3,40 @@ const path = require('path');
 const ElectronServer = require('electron-rpc/server');
 const Menubar = require('menubar');
 const package = require(path.join(__dirname, '../package.json'));
-const docker = require('./docker');
+const Docker = require('./docker');
 
-
-const app = new ElectronServer();
+const server = new ElectronServer();
 
 const menu = Menubar({
 	dir: __dirname,
 	icon: path.join(__dirname, '../resources/IconTemplate.png'),
 	tooltip: `Captain Docker ${package.version}`,
-	width: 200,
-	height: 8 + 2 * 28 + 1 * 10,
+	width: 240,
+	height: 128,
 	windowPosition: 'trayLeft',
+	preloadWindow: true,
 });
 
 menu.on('ready', () => {
 	console.log('app is ready');
 
-	docker.listContainers().then(containers => {
-		console.log(containers);
-	});
+	server.configure(menu.window.webContents);
 
-	app.on('quit', event => {
+	server.on('quit', (event) => {
 		menu.app.quit();
 	});
+
+	server.on('refresh', (event) => {
+		refresh(server);
+	});
 });
+
+menu.on('after-create-window', () => {
+	setTimeout(() => refresh(server), 300);
+});
+
+const refresh = (server) => {
+	Docker.listContainers().then((containers) => {
+		server.send('containers', { containers });
+	});
+};
