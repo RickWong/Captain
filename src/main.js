aa21eacf17098f9225dd3a9b6f96f742896a59cb
Rick Wong <wong@mollie.com>
2016-12-31 14:02:12 +0100

    Can now copy URLs and see up/down status

--- src/main.js
+++ src/main.js
@@ -27,16 +27,40 @@ menu.on('ready', () => {
 	});
 
 	server.on('refresh', (event) => {
-		refresh(server);
+		refresh();
 	});
 });
 
 menu.on('after-create-window', () => {
-	setTimeout(() => refresh(server), 300);
+	setTimeout(() => refresh(), 300);
 });
 
-const refresh = (server) => {
+menu.on('show', () => {
+	refresh();
+});
+
+const refresh = () => {
 	Docker.listContainers().then((containers) => {
-		server.send('containers', { containers });
+		const groups = {};
+
+		for (const container of containers) {
+			const nameParts     = container.name.split('_');
+			const groupName     = (nameParts.length < 2 ? 'nogroup' : nameParts[0]);
+			const containerName = (nameParts.length < 2 ? container.name : container.name.substr(nameParts[0].length + 1));
+
+			container.active = container.status.indexOf('Up') >= 0;
+			container.ports  = ((container.port || '').match(/>([0-9]+)\//) || []).slice(1);
+
+			groups[groupName] = Object.assign(
+				groups[groupName] || {}, 
+				{
+					[containerName]: container,
+				}
+			);
+		}
+
+		server.send('containers', { groups });
+	}).catch((error) => {
+		console.error(error);
 	});
 };
