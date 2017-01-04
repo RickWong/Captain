f5e8451a2a3c996f3bdc7607817ede688b067534
Rick Wong <wong@mollie.com>
2017-01-04 23:29:19 +0100

    Update App Icon.

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const {serverStart} = require("./app/server");
 
 const menubar = Menubar({
 	dir: __dirname,
-	icon: Path.join(__dirname, "../resources/IconTemplate.png"),
+	icon: Path.join(__dirname, "../resources/Icon.png"),
 	index: `file://${Path.join(__dirname, "./gui/gui.html")}`,
 
 	width: 256,
