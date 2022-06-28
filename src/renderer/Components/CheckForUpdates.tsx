63c340a229ac1f9922385d18c782b4b1319f0313
Rick Wong <rick@rygu.org>
2022-06-28 13:08:05 +0200

    If there are no updates, nothing happens. So hide window.

--- src/renderer/Components/CheckForUpdates.tsx
+++ src/renderer/Components/CheckForUpdates.tsx
@@ -1,9 +1,17 @@
 import * as React from "react";
 import { ipcRenderer } from "electron";
+import * as remote from "@electron/remote";
 import { COMMANDS } from "../ipcCommands";
 
-export const CheckForUpdates = () => (
-  <li className="action" onClick={() => ipcRenderer.send(COMMANDS.CHECK_FOR_UPDATES)}>
-    Check for Updates...
-  </li>
-);
+export const CheckForUpdates = () => {
+  const onClick = () => {
+    ipcRenderer.send(COMMANDS.CHECK_FOR_UPDATES);
+    remote.getCurrentWindow().hide();
+  };
+
+  return (
+    <li className="action" onClick={onClick}>
+      Check for Updates...
+    </li>
+  );
+};
