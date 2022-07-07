5e3dcd55f1bdc13e29c77684b84836d50fe46af6
Rick Wong <rick@rygu.org>
2022-07-07 15:45:17 +0200

    Sign with official Developer ID.

--- src/main/checkForUpdates.ts
+++ src/main/checkForUpdates.ts
@@ -8,13 +8,13 @@ autoUpdater.on("error", (event, error) => {
 });
 
 autoUpdater.on("update-not-available", (info) => {
-  debug("captain-check-for-update")("Already up to date", info);
+  debug("captain-check-for-update")("Already up to date");
 
   dialog
     .showMessageBox({
       type: "info",
       message: "Captain is up to date",
-      detail: "It looks like you're already rocking the latest version!",
+      detail: `It looks like you're already rocking the latest version!\n\n(Captain ${info.version})`,
     })
     .catch(debug("captain-check-for-update"));
 });
@@ -23,13 +23,20 @@ autoUpdater.autoDownload = true;
 autoUpdater.autoInstallOnAppQuit = true;
 
 if (process.env.NODE_ENV === "development") {
-  Object.defineProperty(autoUpdater, "currentVersion", { value: "0.0.1" });
-  autoUpdater.updateConfigPath = path.join(__dirname, "../public/app-update.yml");
+  Object.defineProperty(autoUpdater, "currentVersion", { value: "11.0.0" });
+  autoUpdater.updateConfigPath = path.join(__dirname, "../config/app-update.yml");
 }
 
 export const checkForUpdates = async () => {
-  const oldValue = app.isPackaged;
-  Object.defineProperty(app, "isPackaged", { value: true });
-  await autoUpdater.checkForUpdates();
-  Object.defineProperty(app, "isPackaged", { value: oldValue });
+  if (process.env.NODE_ENV === "development") {
+    Object.defineProperty(app, "isPackaged", { value: true });
+  }
+
+  const promise = autoUpdater.checkForUpdatesAndNotify();
+
+  if (process.env.NODE_ENV === "development") {
+    Object.defineProperty(app, "isPackaged", { value: false });
+  }
+
+  return promise;
 };
